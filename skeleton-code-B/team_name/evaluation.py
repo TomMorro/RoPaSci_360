88df8cf3962fd6e4b36e0fd2adb4a54142a0df2f
Thomas Morrison <tdmorrison@student.unimelb.edu.au>
2021-04-26 10:59:05 +1000

    Implemented a basic distance function and began on an evaluation function

---
+++ skeleton-code-B/team_name/evaluation.py
@@ -0,0 +1,39 @@
+# def evaluate_board(board):
+# Things to evaluate
+# Number of tokens each player has
+# A = our_tokens - their_tokens
+# What types of tokens each player has
+# B = our_tokens.rock - their_tokens
+# How close tokens are to being taken
+
+# Number of tokens left to throw
+
+
+def distance_between(token1, token2):
+    """
+    Adapted from code obtained from https://www.redblobgames.com/grids/hexagons/
+    Args:
+        token1: the first token to find distance between
+        token2: the second token to find distance between
+
+    Returns: the distance between the two tokens
+
+    """
+    cube1 = axial_to_cube(token1)
+    cube2 = axial_to_cube(token2)
+    return (abs(cube1[0] - cube2[0]) + abs(cube1[1] - cube2[1]) + abs(cube1[2] - cube2[2])) / 2
+
+
+def axial_to_cube(token):
+    """
+    Adapted from code obtained from https://www.redblobgames.com/grids/hexagons/
+    Args:
+        token: the token in axial coordinates form
+
+    Returns: the token in cube coordinates form
+
+    """
+    x = token[1]
+    z = token[0]
+    y = -x - z
+    return [x, y, z]
